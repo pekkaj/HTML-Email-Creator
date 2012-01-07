e7774ec7d08f0b7028157287f44fe2a93022ced4
Pekka Mattila <pekka.mattila@intoloop.com>
2012-01-07 14:14:14 +0200

    YAML is now required correctly

--- lib/html_email_creator/settings.rb
+++ lib/html_email_creator/settings.rb
@@ -1,3 +1,5 @@
+require 'YAML'
+
 module HtmlEmailCreator
   class Settings
 
--- lib/html_email_creator/version.rb
+++ lib/html_email_creator/version.rb
@@ -1,3 +1,3 @@
 module HtmlEmailCreator
-  VERSION = "1.0.14" unless defined?(::HtmlEmailCreator::VERSION)
+  VERSION = "1.0.15" unless defined?(::HtmlEmailCreator::VERSION)
 end
